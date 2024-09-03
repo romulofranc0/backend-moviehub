72e661013b4bfe281d9a83c9b23edb6bcdf3aa4b
Romulo Maciel <romulo.maciel@wmtrading.com.br>
2024-09-03 12:48:30 -0300

    creating service

---
+++ src/main/java/com/moviehub/backend_moviehub/services/MovieService.java
@@ -0,0 +1,29 @@
+package com.moviehub.backend_moviehub.services;
+
+import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.moviehub.backend_moviehub.clients.OmdbApiCLient;
+import com.moviehub.backend_moviehub.models.MovieDetails;
+import com.moviehub.backend_moviehub.models.Search;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private OmdbApiCLient omdbApiCLient;
+
+    @Autowired
+    private Environment environment;
+
+    public MovieDetails getDetails(String title) {
+        String apiKey = environment.getProperty("omdb.api.key");
+        return omdbApiCLient.movieDetails(title, apiKey);
+    }
+
+    public Search getMovies(String imdbId) {
+        String apiKey = environment.getProperty("omdb.api.key");
+        return omdbApiCLient.searchMovies(imdbId, apiKey);
+    }
+}
