021ab6da51f94a9e5d7b8c38120d3ef6b81b9736
Romulo Maciel <romulo.maciel@wmtrading.com.br>
2024-09-03 14:43:59 -0300

    creating userService (need to create repository and DB)

---
+++ src/main/java/com/moviehub/backend_moviehub/services/UserService.java
@@ -0,0 +1,11 @@
+package com.moviehub.backend_moviehub.services;
+
+import com.moviehub.backend_moviehub.models.User;
+
+public class UserService {
+    
+    // public User createUser(User user){};
+    // public User getUserById(String id){};
+    // public User updateUser(User user){};
+    // public void deleteUser(String id){};
+}
