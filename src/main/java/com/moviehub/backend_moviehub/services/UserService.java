418dc72b82e674618c89b8a7840a64c85660c176
Romulo Maciel <romulo.maciel@wmtrading.com.br>
2024-09-03 14:56:15 -0300

    UserController

--- src/main/java/com/moviehub/backend_moviehub/services/UserService.java
+++ src/main/java/com/moviehub/backend_moviehub/services/UserService.java
@@ -4,7 +4,9 @@ import com.moviehub.backend_moviehub.models.User;
 
 public class UserService {
     
-    // public User createUser(User user){};
+    public User createUser(User user){
+        return null;
+    };
     // public User getUserById(String id){};
     // public User updateUser(User user){};
     // public void deleteUser(String id){};
