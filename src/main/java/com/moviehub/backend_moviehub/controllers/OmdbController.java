c879724208b9080ddeed7e5facd1d0a38d2999cc
Romulo Maciel <romulo.maciel@wmtrading.com.br>
2024-09-03 12:57:39 -0300

    placing autowired

--- src/main/java/com/moviehub/backend_moviehub/controllers/OmdbController.java
+++ src/main/java/com/moviehub/backend_moviehub/controllers/OmdbController.java
@@ -1,20 +1,20 @@
 package com.moviehub.backend_moviehub.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.moviehub.backend_moviehub.models.MovieDetails;
 import com.moviehub.backend_moviehub.models.Search;
 import com.moviehub.backend_moviehub.services.MovieService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class OmdbController {
 
+    @Autowired
     private MovieService movieService;
-
 
     @GetMapping("/movies")
     public Search getMovies(@RequestParam String title) {
