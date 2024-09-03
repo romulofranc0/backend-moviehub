ba5c0ea87e382610a30e2437221aa560307b7973
Romulo Maciel <romulo.maciel@wmtrading.com.br>
2024-09-03 10:10:01 -0300

    criando projeto backend spring

---
+++ src/main/java/com/moviehub/backend_moviehub/BackendMoviehubApplication.java
@@ -0,0 +1,13 @@
+package com.moviehub.backend_moviehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMoviehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMoviehubApplication.class, args);
+	}
+
+}
