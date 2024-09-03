266cfc4c88accb85c276127099a6fc9a556dd62e
Romulo Maciel <romulo.maciel@wmtrading.com.br>
2024-09-03 14:43:19 -0300

    creating user in models

---
+++ src/main/java/com/moviehub/backend_moviehub/models/User.java
@@ -0,0 +1,13 @@
+package com.moviehub.backend_moviehub.models;
+
+import java.util.List;
+
+public class User {
+
+    private String userId;
+    private String name;
+    private String email;
+    private String googleId;
+    private List<User> following;
+
+    }
