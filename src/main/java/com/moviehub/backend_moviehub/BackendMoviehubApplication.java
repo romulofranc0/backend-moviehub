3b0196a5d42f5fb9ed6871e38861e60ae6469207
Romulo Maciel <romulo.maciel@wmtrading.com.br>
2024-09-03 12:47:40 -0300

    enabling feignClients

--- src/main/java/com/moviehub/backend_moviehub/BackendMoviehubApplication.java
+++ src/main/java/com/moviehub/backend_moviehub/BackendMoviehubApplication.java
@@ -2,8 +2,10 @@ package com.moviehub.backend_moviehub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BackendMoviehubApplication {
 
 	public static void main(String[] args) {
